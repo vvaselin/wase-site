cc28e5c9e2042f5fb56ea002f19f87ddadb20412
vvaselin <0pw3292219w259d@gmail.com>
2026-08-21 20:49:41 +0900

    ブログ書けるように

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const posts = defineCollection({
+  loader: glob({
+    base: "./src/content/posts",
+    pattern: "**/*.md",
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+
+    publishedAt: z.coerce.date(),
+    updatedAt: z.coerce.date().optional(),
+
+    category: z.string(),
+    tags: z.array(z.string()).default([]),
+
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  posts,
+};
